f23030abfa14338579e09be37c4c131372b8e641
Saulo Sena <saulobdsena@gmail.com>
2026-05-24 17:26:45 -0300

    feat: new dependence

--- src/conversor/core.clj
+++ src/conversor/core.clj
@@ -1,11 +1,24 @@
 (ns conversor.core
   (:gen-class))
 
+;Importand o a função parse-opts do clj.tools
+(ns conversor.core
+(:require [clojure.tools.cli :refer [parse-opts]])
+(:gen-class))
+
+
+;(defn- valores-em [arg]
+; (cond
+; (.startsWith arg "--de=")
+;  {:de (subs arg 5)}
+; (.startsWith arg "--para=")
+;  {:para (subs arg 7)}
+; :else {}))
+
+
+
 (defn -main
   "Meu proprio main"
   [& args]
-  (println "Temos" (count args) "argumentos")
-  (println "Os argumentos são:"args)
-  (println "Saulo made in clojure")
-  
+  (println "os argumentos são:" (map valores-em args))
   )
