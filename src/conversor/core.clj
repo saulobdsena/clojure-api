f1a174be254d2663b8c2aedbcfb4e6f719f8ef44
Saulo Sena <saulobdsena@gmail.com>
2026-05-24 12:41:37 -0300

    first commit

---
+++ src/conversor/core.clj
@@ -0,0 +1,11 @@
+(ns conversor.core
+  (:gen-class))
+
+(defn -main
+  "Meu proprio main"
+  [& args]
+  (println "Temos" (count args) "argumentos")
+  (println "Os argumentos são:"args)
+  (println "Saulo made in clojure")
+  
+  )
