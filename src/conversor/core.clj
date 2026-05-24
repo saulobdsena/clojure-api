24a12eddbf42f89742a457a48a9fc40bfd867cd6
Saulo Sena <saulobdsena@gmail.com>
2026-05-24 17:50:51 -0300

    feat: new dependence

--- src/conversor/core.clj
+++ src/conversor/core.clj
@@ -15,10 +15,14 @@
 ;  {:para (subs arg 7)}
 ; :else {}))
 
-
+(def opcoes-do-programa
+[["-d" "--de moeda base" "moeda base para conversão"
+:default "eur"]
+["-p" "--para moeda distino"
+      "moeda a qual queremos saber o valor"]])
 
 (defn -main
   "Meu proprio main"
   [& args]
-  (println "os argumentos são:" (map valores-em args))
-  )
+  (prn "as opções são" (:options
+  (parse-opts args opcoes-do-programa))))
